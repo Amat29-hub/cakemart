5cf84aa5035c4343843b2c3a6a4649780a84870e
akuiniwinda <windanurazizh@gmail.com>
2025-10-01 16:51:17 +0700

    Tambah frontend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\MartLandingFrontendController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Mart
+Route::get('/mart', [MartLandingFrontendController::class, 'index']);
