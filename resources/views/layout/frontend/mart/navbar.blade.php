549901f853c259add6dec439c6ae90c231448f62
akuiniwinda <windanurazizh@gmail.com>
2025-10-03 10:19:35 +0700

    edit isi konten frontend

--- resources/views/layout/frontend/mart/navbar.blade.php
+++ resources/views/layout/frontend/mart/navbar.blade.php
@@ -5,7 +5,7 @@
           <div class="col-sm-4 col-lg-3 text-center text-sm-start">
             <div class="main-logo">
               <a href="index.html">
-                <img src="{{ asset('assetsfrontend/images/logo.png') }}" alt="logo" class="img-fluid">
+                <img src="{{ asset('assetsfrontend/images/CAKEMART.png') }}" alt="logo" class="img-fluid" style="width:241px; height:54px;">
               </a>
             </div>
           </div>
